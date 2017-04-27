591391b99bdced86497663ed8f41b12413d8a768
BenjaminLongobardi <Benjamin.Longobardi@gmail.com>
2017-04-27 14:18:38 -0400

    started coding gridclass, had to go to stats

--- src/com/winners/GridClass.java
+++ src/com/winners/GridClass.java
@@ -4,4 +4,11 @@ package com.winners;
  * Created by benco on 4/27/2017.
  */
 public class GridClass {
+    private int mRows;// # of rows
+    private int mCols;// # of cols
+    private int [] mFreeRows;// # of unassigned points in each row
+    private int [] mFreeCols;// # of unassigned points in each col
+    private PointClass [] mGrid;// the M16 keypad
+
+
 }
